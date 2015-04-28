29a20c821c6e0ac9d4c8cc031faa5337078ee8e0
Pablo Quinci <pablo.quinci@gmail.com>
2015-04-28 14:35:40 -0300

    Ejemplo de Threads

--- Prueba TP/src/Prueba.c
+++ Prueba TP/src/Prueba.c
@@ -13,6 +13,49 @@
 
 void CambiarE_S(char *, char *);
 
+// Ejemplo threads
+
+pthread_t thread1;
+	pthread_t thread2;
+
+	const char *message1 = "Thread 1";
+
+	const char *message2 = "Thread 2";
+
+	int iret1, iret2;
+
+	/* Create independent threads each of which will execute function */
+
+	iret1 = pthread_create(&thread1, NULL, print_message_function,
+			NULL);
+
+	if (iret1)
+
+	{
+
+		fprintf(stderr, "Error - pthread_create() return code: %d\n", iret1);
+
+		exit(EXIT_FAILURE);
+
+	}
+
+	iret2 = pthread_create(&thread2, NULL, print_message_function,NULL);
+
+	if (iret2)
+
+	{
+
+		fprintf(stderr, "Error - pthread_create() return code: %d\n", iret2);
+
+		exit(EXIT_FAILURE);
+
+	}
+	pthread_join(thread1, NULL);
+	pthread_join(thread2, NULL);
+
+
+
+
 int main(void) {
 	char entrada[] = "/tmp/Entrada.txt", salida[] ="/tmp/Salida.txt"; //Crear los archivos .txt en la ruta
 	char palabra;													  //Entrada.txt tiene que tener valores
@@ -22,6 +65,7 @@ int main(void) {
 
 	while ((palabra = getchar()) != EOF) printf("%c\n", palabra);
 
+
 	return EXIT_SUCCESS;
 }
 
@@ -30,3 +74,5 @@ void CambiarE_S(char * DirEntrada, char * DirSalida){
 	freopen(DirSalida, "w", stdout);
 	freopen(DirEntrada, "r", stdin);
 }
+
+
