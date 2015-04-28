ff5be129cc84ee9082aede7d01701017b6647acb
mpedulla <matias_pedulla@live.com.ar>
2015-04-28 10:10:20 -0300

    Mejora en la captura de valores por pantalla

--- FileSystem/src/FileSystem.c
+++ FileSystem/src/FileSystem.c
@@ -2,7 +2,7 @@
  ============================================================================
  Name        : FileSystem.c
  Author      : Mati
- Version     : 0.1
+ Version     : 0.2
  Copyright   : Your copyright notice
  Description : Consola para FileSystem
  ============================================================================
@@ -10,6 +10,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /*Funciones postas*/
 void Formatear(){
@@ -74,61 +75,92 @@ void MostrarOpcionesNodos();
 
 /*Funciones de acciones*/
 void AccionArchivos(){
-	char Accion;
-	fflush(stdin);
-	Accion= getchar() - 48;
-	getchar();
+	char AccionString[15];
+	int Accion;
+
+	gets(AccionString);
+
+	if(AccionString[1] != '\0')
+		Accion=0;
+	else
+		Accion = AccionString[0] - 48;
+
 	switch(Accion){
 	case 1: EliminarArchivo(); break;
 	case 2: RenombrarArchivo(); break;
 	case 3: MoverArchivo(); break;
 	case 4: MostrarConsola(); break;
-	default : printf("Error Arch %d", Accion);
+	default: printf("\nLa opción es errónea. Vuelva a ingresar una opción\n\n"); MostrarOpcionesArchivos(); AccionArchivos(); break;
 	}
 }
 void AccionDirectorios(){
-	char Accion;
-	fflush(stdin);
+	char AccionString[15];
+	int Accion;
 
-	Accion= getchar() - 48;
-	getchar();
+	gets(AccionString);
+
+	if(AccionString[1] != '\0')
+		Accion=0;
+	else
+		Accion = AccionString[0] - 48;
+
 	switch(Accion){
 	case 1: CrearDirectorio(); break;
 	case 2: ELiminarDirectorio(); break;
 	case 3: RenombrarDirectorio(); break;
 	case 4: MoverDirectorio(); break;
 	case 5: MostrarConsola(); break;
+	default: printf("\nLa opción es errónea. Vuelva a ingresar una opción\n\n"); MostrarOpcionesDirectorios(); AccionDirectorios(); break;
 	}
 }
 void AccionBloques(){
-	char Accion;
-	fflush(stdin);
-	Accion= getchar() - 48;
-	getchar();
+	char AccionString[15];
+	int Accion;
+
+	gets(AccionString);
+
+	if(AccionString[1] != '\0')
+		Accion=0;
+	else
+		Accion = AccionString[0] - 48;
+
 	switch(Accion){
 	case 1: VerBloque(); break;
 	case 2: BorrarBloque(); break;
 	case 3: CopiarBloque(); break;
 	case 4: MostrarConsola(); break;
+	default: printf("\nLa opción es errónea. Vuelva a ingresar una opción\n\n"); MostrarOpcionesBloques(); AccionBloques(); break;
 	}
 }
 void AccionNodos(){
-	char Accion;
-	fflush(stdin);
-	Accion= getchar() - 48;
-	getchar();
+	char AccionString[15];
+	int Accion;
+
+	gets(AccionString);
+
+	if(AccionString[1] != '\0')
+		Accion=0;
+	else
+		Accion = AccionString[0] - 48;
+
 	switch(Accion){
 		case 1: AgregarNodo(); break;
 		case 2: EliminarNodo(); break;
 		case 3: MostrarConsola(); break;
+		default: printf("\nLa opción es errónea. Vuelva a ingresar una opción\n\n"); MostrarOpcionesNodos(); AccionNodos(); break;
 	}
 }
 void AccionConsola(){
-	char Accion;
-	fflush(stdin);
+	char AccionString[15];
+	int Accion;
 
-	Accion= getchar()- 48;
-	getchar();
+	gets(AccionString);
+
+	if(AccionString[1] != '\0')
+		Accion=0;
+	else
+		Accion = AccionString[0] - 48;
+
 	switch(Accion){
 	case 1: Formatear(); break;
 	case 2: MostrarOpcionesArchivos(); AccionArchivos(); break;
@@ -139,7 +171,7 @@ void AccionConsola(){
 	case 7: MostrarOpcionesBloques(); AccionBloques(); break;
 	case 8: MostrarOpcionesNodos(); AccionNodos(); break;
 	case 9: return;
-	default : printf("\nOpción errónea. Vuelva a ingresar una opción %d\n\n", Accion); MostrarConsola();
+	default : printf("\nLa opción es errónea. Vuelva a ingresar una opción\n\n"); MostrarConsola(); break;
 	}
 
 }
@@ -147,7 +179,8 @@ void AccionConsola(){
 
 /*Funiones para mostrar en pantalla*/
 void MostrarConsola (){
-	puts("1- Formatear el MDFS\n"
+	puts("Pantalla Principal\n"
+		"1- Formatear el MDFS\n"
 		"2- Eliminar/Renombrar/Mover archivo\n"
 		"3- Crear/Eliminar/Renombrar/Mover directorio\n"
 		"4- Copiar un archivo local al MDFS\n"
@@ -160,25 +193,29 @@ void MostrarConsola (){
 	AccionConsola();
 }
 void MostrarOpcionesArchivos(){
-	puts("1- Eliminar archivo\n"
+	puts("Opciones Archivos\n"
+		"1- Eliminar archivo\n"
 		"2- Renombrar archivo\n"
 		"3- Mover archivo\n"
 		"4- Volver\n");
 }
 void MostrarOpcionesDirectorios(){
-	puts("1- Crear directorio \n"
+	puts("Opciones Directorios\n"
+		"1- Crear directorio \n"
 		"2- Eliminar directorio\n"
 		"3- Renombrar directorio\n"
 		"4- Mover directorio\n"
 		"5- Volver\n");
 }
 void MostrarOpcionesNodos(){
-	puts("1- Agregar Nodo\n"
+	puts("Opciones Nodos\n"
+		"1- Agregar Nodo\n"
 		"2- Eliminar Nodo\n"
 		"3- Volver\n");
 }
 void MostrarOpcionesBloques(){
-	puts("1- Ver bloque\n"
+	puts("Opciones Bloques\n"
+		"1- Ver bloque\n"
 		"2- Borrar bloque\n"
 		"3- Copiar bloque\n"
 		"4- Volver\n");
