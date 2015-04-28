625e873fa9fc47c757fe485cea9a51b11deaf00b
mpedulla <matias_pedulla@live.com.ar>
2015-04-28 12:47:47 -0300

    Funcion E/S - Prueba

--- Prueba TP/src/Prueba.c
+++ Prueba TP/src/Prueba.c
@@ -11,7 +11,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void CambiarE_S(char *, char *);
+
 int main(void) {
-	puts("Hola"); /* prints !!!Hello World!!! */
+	char entrada[] = "/tmp/Entrada.txt", salida[] ="/tmp/Salida.txt"; //Crear los archivos .txt en la ruta
+	char palabra;													  //Entrada.txt tiene que tener valores
+
+	CambiarE_S(entrada, salida);
+	printf("Texto de Prueba\n");
+
+	while ((palabra = getchar()) != EOF) printf("%c\n", palabra);
+
 	return EXIT_SUCCESS;
+}
+
+
+void CambiarE_S(char * DirEntrada, char * DirSalida){
+	freopen(DirSalida, "w", stdout);
+	freopen(DirEntrada, "r", stdin);
 }
